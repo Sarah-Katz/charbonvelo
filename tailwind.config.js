1aa2a6d8f6442d1a0f4ec025126ff4f6e9471e1f
SauroMatheo <sauromatheo@gmail.com>
2024-12-26 16:13:54 +0100

    feat: Basic page design for articles
    Still needs refining, and to go back to mobile before that.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
               primary: '#002043',   // Couleur principale
               secondary: '#D3B04D', // Couleur secondaire
               accent: '#CE3939',    // Couleur d'accentuation
+              background: '#FEFEFE', // Couleur d'arrière-plan
+              text: '#222222', // Couleur de texte
+              white_text: "#FEFEFE" // Texte sur fond sombre
           },
       },
   },
