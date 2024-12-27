ab0b701dea855a874e682ad20d10387306642c19
CestPasFo <c.jaworski12@hotmail.fr>
2024-12-27 12:07:24 +0100

    Feat : Adding a redirect to home page in admin panel

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -17,30 +17,22 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 class DashboardController extends AbstractDashboardController
 {
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
         return $this->render('admin/dashboard.html.twig', [
             'some_variable' => 'Some value']);
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -50,14 +42,18 @@ class DashboardController extends AbstractDashboardController
     }
 
     public function configureMenuItems(): iterable
-    {
+    {        
+        yield MenuItem::linkToUrl('Retour à l\'accueil', 'fa fa-arrow-left', $this->router->generate('app_home'));
+        
+        // Ajout d'une ligne de séparation
+        yield MenuItem::section();
+        
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
         yield MenuItem::linkToCrud('Article', 'fas fa-newspaper', Article::class);
         yield MenuItem::linkToCrud('Category', 'fas fa-bars', Category::class);
         yield MenuItem::linkToCrud('Message', 'fas fa-message', Message::class);
         yield MenuItem::linkToCrud('Stage', 'fas fa-road', Stage::class);
         yield MenuItem::linkToCrud('Subject', 'fas fa-envelope', Subject::class);
         yield MenuItem::linkToCrud('Users', 'fas fa-users', User::class);
-    }
+    }    
 }
