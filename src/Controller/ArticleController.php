75aaef45e67a08a66d2cf6ef038744999a426d59
SauroMatheo <sauromatheo@gmail.com>
2024-12-31 12:16:11 +0100

    feat: Basic like logic and like button

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -21,5 +23,46 @@ class ArticleController extends AbstractController
         return $this->render('article/index.html.twig', [
             'article' => $article,
         ]);
+    }
+
+    #[Route('/like/article/{id}', name: 'like_article', methods: ["POST"])]
+    public function likeArticle(
+        int $id,
+        ArticleRepository $articleRepo,
+        EntityManagerInterface $entityManager
+        ): Response
+    {
+        $user = $this->getUser();
+
+        if (isset($user)) {
+            $article = $articleRepo->find($id);
+
+            if ($user->getLikedArticle()->contains($article)) {
+                $user->removeLikedArticle($article);
+            } else {
+                $user->addLikedArticle($article);
+            }
+
+            $entityManager->persist($user);
+            $entityManager->persist($article);
+
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();
+        } else {
+            // TODO: Do something special if the user isn't logged in
+        }
+        return $this->redirectToRoute(route: "app_article", parameters: ["id"=> $id]);
+    }
+
+    #[Route('/like/message/{id}', name: 'like_article_comment', methods: ["POST"])]
+    public function likeComment(
+        int $id,
+        ArticleRepository $articleRepo
+        ): Response
+    {
+        // TODO: Logic
+        $article = $articleRepo->find($id);
+
+        return $this->redirectToRoute("app_article");
     }
 }
