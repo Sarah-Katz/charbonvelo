913e7aecb24e59b5d1eb2556e9b5a757c45db739
Sarah-katz <sarahkatz59@gmail.com>
2025-01-02 14:47:15 +0100

    feat(Message): added POST route to controller

--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
 use App\Entity\Subject;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -22,6 +24,29 @@ class SubjectController extends AbstractController
 
         return $this->render('subject/show.html.twig', [
             'controller_name' => 'SubjectController',
+            'subject'         => $subject,
+        ]);
+    }
+
+    #[Route('/subject/{id}/newMessage', name: 'app_message_new', methods: ['POST'])]
+    public function newMessage(int $id, Request $request): Response
+    {
+        $subject = $this->em->getRepository(Subject::class)->find($id);
+        $message = new Message();
+        $form = $this->createForm(MessageFormType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message->setSubject($subject);
+            $this->em->persist($message);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_subject_show', ['id' => $id]);
+        }
+
+        return $this->render('subject/newMessage.html.twig', [
+            'controller_name' => 'SubjectController',
+            'form'            => $form->createView(),
             'subject'         => $subject,
         ]);
     }
