43a0e560cc60c387b6408c3a0625b332cf2e4825
SauroMatheo <sauromatheo@gmail.com>
2025-01-06 13:59:11 +0100

    feat: Implement dynamic slider images in home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Image;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->em = $entityManager;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(ArticleRepository $articleRepository): Response
     {
+        $sliderImages = $this->em->getRepository(Image::class)->findBy(['isOnSlider' => true]);
         $latestArticles = $articleRepository->findBy([], ['date' => 'DESC'], 6);
 
         return $this->render('home.html.twig', [
             'controller_name' => 'HomeController',
             'latestArticles' => $latestArticles,
+            'sliderImages' => $sliderImages
         ]);
     }
 }
